e3e0103e4736a7c708f828a48b574ec65f14fc41
anusha <anusha.hanagandi@one2n.in>
2025-12-03 18:56:46 +0530

    Add student repo

---
+++ src/main/java/com/example/student_app/repository/StudentRepository.java
@@ -0,0 +1,7 @@
+package com.example.student_app.repository;
+
+import com.example.student_app.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+}
