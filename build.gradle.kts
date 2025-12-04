ba6d2abb997400a3080534c970dd76e959d9b545
anusha <anusha.hanagandi@one2n.in>
2025-12-04 11:04:20 +0530

    Enable health endpoint

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
 
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
@@ -31,6 +32,7 @@ dependencies {
     //flyway for migrations
     implementation("org.flywaydb:flyway-core:11.18.0")
     runtimeOnly("org.flywaydb:flyway-database-postgresql:11.18.0")
+
     //lombok
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
