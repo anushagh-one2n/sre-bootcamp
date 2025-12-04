425ff4f38d1ae3e444ef8c130fcfeb8a7700fbb0
anusha <anusha.hanagandi@one2n.in>
2025-12-04 11:06:34 +0530

    Add request logs in controller

--- src/main/java/com/example/student_app/controller/StudentController.java
+++ src/main/java/com/example/student_app/controller/StudentController.java
@@ -3,6 +3,8 @@ package com.example.student_app.controller;
 import com.example.student_app.dto.StudentRequest;
 import com.example.student_app.dto.StudentResponse;
 import com.example.student_app.service.StudentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +16,7 @@ import java.util.List;
 public class StudentController {
 
     private final StudentService service;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     public StudentController(StudentService service) {
         this.service = service;
@@ -21,28 +24,33 @@ public class StudentController {
 
     @PostMapping
     public ResponseEntity<StudentResponse> create(@RequestBody StudentRequest request) {
+        logger.info("REQ - Create student");
         StudentResponse response = service.create(request);
         return ResponseEntity.ok(response);
     }
 
     @GetMapping
     public ResponseEntity<List<StudentResponse>> getAll() {
+        logger.info("REQ - Get all students");
         return ResponseEntity.ok(service.getAll());
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<StudentResponse> getById(@PathVariable Long id) {
+        logger.info("REQ - Get student by ID");
         return ResponseEntity.ok(service.getById(id));
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<StudentResponse> update(@PathVariable Long id, @RequestBody StudentRequest request) {
+        logger.info("REQ - Update student");
         return ResponseEntity.ok(service.update(id, request));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         service.delete(id);
+        logger.info("REQ - Delete student by ID");
         return ResponseEntity.noContent().build();
     }
 }
