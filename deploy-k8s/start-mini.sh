a3e0e0b07012ce3a6bd7e57011c8b4ca1d35cb0f
anusha <anusha.hanagandi@one2n.in>
2025-12-15 11:19:39 +0530

    Update shell script to include driver

--- deploy-k8s/start-mini.sh
+++ deploy-k8s/start-mini.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-set -e pipefail
+set -euo pipefail
 
-minikube start --nodes=3 -p minii-cluster
+minikube start --driver="docker" --nodes=3 -p minii-cluster
+
 kubectl label node minii-cluster type=application
 kubectl label node minii-cluster-m02 type=database
 kubectl label node minii-cluster-m03 type=dependent_service
