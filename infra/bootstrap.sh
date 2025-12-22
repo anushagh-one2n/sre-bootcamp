095436b57537db55a2b3d82be5b5be42628770b9
anusha <anusha.hanagandi@one2n.in>
2025-12-22 12:18:06 +0530

    Clean up bootstrap script

--- infra/bootstrap.sh
+++ infra/bootstrap.sh
@@ -2,106 +2,75 @@
 
 set -euo pipefail
 
-#minikube start --driver="docker" --nodes=3 -p minii-cluster
-#
-#kubectl label node minii-cluster type=application
-#kubectl label node minii-cluster-m02 type=database
-#kubectl label node minii-cluster-m03 type=dependent_service
+minikube start --driver="docker" --nodes=3 -p minii-cluster
 
-#
-##minikube addons enable ingress -p minii-cluster
-##
-#echo "======================================================"
-#echo " Bootstrapping Cluster (Vault + ESO + Infra + App)    "
-#echo "======================================================"
-#
-################################################
-## 1. INSTALL EXTERNAL SECRETS OPERATOR (CRDs)
-################################################
-#echo "[1/7] Installing External Secrets Operator..."
-#
-#helm repo add external-secrets https://charts.external-secrets.io >/dev/null 2>&1
-#helm repo update >/dev/null 2>&1
-#
-#helm upgrade --install external-secrets external-secrets/external-secrets \
-#  -n external-secrets --create-namespace
-#
-#
-################################################
-## 2. INSTALL VAULT VIA HELM (NOT CONFIGURED YET)
-################################################
-echo "[2/7] Installing Vault..."
+kubectl label node minii-cluster type=application
+kubectl label node minii-cluster-m02 type=database
+kubectl label node minii-cluster-m03 type=dependent_service
+
+minikube addons enable ingress -p minii-cluster
+
+echo "======================================================"
+echo " Bootstrapping Cluster (Vault + ESO + Infra + App)    "
+echo "======================================================"
+###
+##################################################
+#### 1. INSTALL EXTERNAL SECRETS OPERATOR (CRDs)
+##################################################
+echo "[1/6] Installing External Secrets Operator..."
+
+helm repo add external-secrets https://charts.external-secrets.io >/dev/null 2>&1
+helm repo update >/dev/null 2>&1
+
+helm upgrade --install external-secrets external-secrets/external-secrets \
+  -n external-secrets --create-namespace
+
+
+##################################################
+#### 2. INSTALL VAULT VIA HELM (NOT CONFIGURED YET)
+##################################################
+echo "[2/6] Installing Vault..."
 helm repo add hashicorp https://helm.releases.hashicorp.com >/dev/null 2>&1
 helm repo update >/dev/null 2>&1
 
 helm upgrade --install vault hashicorp/vault \
   -n vault --create-namespace \
-  -f vault/values-ha.yaml
-#
-#
+  -f vault/values-dev.yaml
+
+
+#################################################
+### 3. INSTALL INFRA CHART (ClusterSecretStore)
+#################################################
+echo "[3/6] Installing infra chart..."
+
+helm upgrade --install infra ../charts/infra \
+  -n infra --create-namespace
+
 ################################################
-## 3. PREPARE DOCKERHUB CREDENTIALS SECRET
+## 4. WAIT FOR VAULT TO BE READY
 ################################################
-#echo "[3/7] Creating DockerHub registry secret..."
-#
-## Expected ENV variables:
-##   DOCKERHUB_USERNAME
-##   DOCKERHUB_PASSWORD
-##   DOCKERHUB_EMAIL (optional)
-#
-#
-#if [[ -z "$DOCKERHUB_USERNAME" || -z "$DOCKERHUB_PASSWORD" ]]; then
-#  echo "ERROR: DOCKERHUB_USERNAME and DOCKERHUB_PASSWORD must be set."
-#  exit 1
-#fi
-#
-#
-#kubectl create namespace student-api --dry-run=client -o yaml | kubectl apply -f -
-#
-#
-#
-#kubectl delete secret dockerhub-creds -n student-api --ignore-not-found >/dev/null 2>&1
-#kubectl create secret docker-registry dockerhub-creds \
-#  --docker-username="$DOCKERHUB_USERNAME" \
-#  --docker-password="$DOCKERHUB_PASSWORD" \
-#  --docker-email="${DOCKERHUB_EMAIL:-dev@example.com}" \
-#  -n student-api
-
-
-###############################################
-# 4. INSTALL INFRA CHART (ClusterSecretStore)
-###############################################
-#echo "[4/7] Installing infra chart..."
-#
-#helm upgrade --install infra ../charts/infra \
-#  -n infra --create-namespace
-
-
-###############################################
-# 5. WAIT FOR VAULT TO BE READY
-###############################################
-echo "[5/7] Waiting for vault-0 pod to be ready..."
+echo "[4/6] Waiting for vault-0 pod to be ready..."
 
 kubectl wait --for=condition=Ready pod/vault-0 \
   -n vault --timeout=180s
 
 
 ###############################################
-# 6. RUN VAULT CONFIGURATION SCRIPT
+# 5. RUN VAULT CONFIGURATION SCRIPT
 ###############################################
-echo "[6/7] Running Vault configuration apply.sh..."
+echo "[5/6] Running Vault configuration apply.sh..."
 
 chmod +x vault/apply.sh
 ./vault/apply.sh
 
 
 ###############################################
-# 7. INSTALL APPLICATION
+# 6. INSTALL APPLICATION
 ###############################################
-echo "[7/7] Installing student-api chart..."
+echo "[6/6] Installing student-api chart..."
 
-helm upgrade --install student-api charts/student-api \
-  -n student-api --create-namespace \
+helm install student-app ../charts/student-app \
+  -n student-api-staging --create-namespace \
   --set image.pullSecrets[0]=dockerhub-creds
 
 
